c7bab5022ef93a13f33b8939dca21ded60706540
shuvamoy <shuvamoy1983@gmail.com>
2018-01-05 01:16:58 -0600

    numpy code changes

--- Numpy.py
+++ Numpy.py
@@ -1,12 +1,44 @@
 import numpy as np
 
 ## to get a list of values
-v = np.arange(15)
+v = np.arange(25)
 print(v)
 
 ## To form a matrix with reshape
-r =v.reshape(3,5)
+r =v.reshape(5,5)
 print(r)
 
 ##the number of axes (dimensions) of the array.
-print(v.ndim)
+print(r.ndim)
+
+## To see the dimensions of the array. shape will be (n,m)
+print(r.shape)
+
+## an object describing the type of the elements in the array.
+print(r.dtype)
+
+## the size in bytes of each element of the array. one of type complex64 has itemsize 4 (=64/8).
+print("itemsize",r.itemsize)
+
+## the total number of elements of the array.
+print(r.size)
+
+## Array Creation
+print("_____________________________________")
+print("Array creation")
+b = np.array([1.1,2.3,4.5])
+print(b.dtype)
+
+print("Array creation for two dimentional array")
+d = np.array([(1,2,3),(4,6,7)])
+
+## to create complex data , use dtype = complex
+c = np.array([(1,2,3),(4,6,7)], dtype=complex)
+print(c)
+print(d)
+
+## to print zero value in matrix
+c = np.zeros((5,5))
+print(c)
+
+
