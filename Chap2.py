abfef5c64ea58eb731e513c6dae83c23323f70e1
shuvamoy <shuvamoy1983@gmail.com>
2018-01-11 23:05:33 -0600

    Chap2 Added

--- Chap2.py
+++ Chap2.py
@@ -8,3 +8,20 @@ import numpy as np
 v = np.array([1,2,3])
 c= np.asarray(v)
 print(c)
+
+# ndarray from tuple
+x =(1,2,3)
+b = np.asarray(x)
+print(b)
+
+##numpy.fromiter - must be iterator object. iter is used to convert to iterable object
+##This function builds an ndarray object from any iterable object.
+list = range(5)
+it = iter(list)
+print(it)
+
+c = np.fromiter(it,dtype=float)
+print(c)
+
+
+
